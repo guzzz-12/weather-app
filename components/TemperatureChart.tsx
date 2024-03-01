40a1547c3349c1b55f933257f347d6876d873545
unknown <jegq.dev.projects@gmail.com>
2024-03-01 15:54:37 -0400

    Cambio de estilos del card de temperatura

--- components/TemperatureChart.tsx
+++ components/TemperatureChart.tsx
@@ -30,46 +30,51 @@ const TemperatureChart = ({data, isLoading, error}: Props) => {
     const isNightIcon = data.weather[0].icon.endsWith("n");
 
     return (
-      <div className="flex flex-col justify-between items-center w-full max-w-[250px] aspect-square py-2 flex-shrink-0 border dark:border-neutral-700 rounded-lg bg-neutral-100 dark:bg-neutral-800">
+      <div className="flex flex-col justify-between items-center w-full max-w-[250px] aspect-square py-2 flex-shrink-0 border dark:border-neutral-700 rounded-lg bg-neutral-100 dark:bg-neutral-950">
         <div className="w-full">
-          <div className="flex justify-between items-center w-full mb-2 px-3">
-            <p className="text-sm text-neutral-700 dark:text-white">
+          <div className="flex justify-between items-center w-full mb-2 px-3 pb-2 border-b">
+            <p className="text-xs text-neutral-700 dark:text-white">
               {moment().format("dddd")}
             </p>
-            <p className="text-sm text-neutral-700 dark:text-white">
+            <p className="text-xs text-neutral-700 dark:text-white">
               Local Time: {moment().utcOffset(data.timezone / 60).format("HH:mm")}
             </p>
           </div>
   
           <div className="flex justify-start items-center gap-1 w-full px-3">
             <MapPin className="w-4 h-4 opacity-65" />
-            <p className="text-sm font-semibold text-neutral-700 dark:text-white">
+            <p className="font-semibold text-neutral-700 dark:text-white">
               {data.name}
             </p>
           </div>
         </div>
   
-        <p className="text-6xl text-center font-bold">
-          {Math.round(data.main.temp)}°C
-        </p>
-  
-        <div className="w-full ml-4">
-          <div className="flex justify-start items-center gap-1 mb-2">
-            <img
-              className={cn("block w-9 h-7 object-cover object-center border rounded-sm bg-neutral-400 dark:border-neutral-700 dark:bg-transparent", isNightIcon && "bg-transparent dark:bg-neutral-300")}
-              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-            />
-            <p className="text-sm">
-              {data.weather[0].main}
+        <div className="flex flex-col gap-1">
+          <p className="text-6xl text-center font-bold">
+            {Math.round(data.main.temp)}°C
+          </p>
+
+          <div className="flex justify-center items-center gap-2 w-full text-xs">
+            <p>
+              Min: {Math.round(data.main.temp_min)}°C
+            </p>
+
+            <span>|</span>
+
+            <p>
+              Max: {Math.round(data.main.temp_max)}°C
             </p>
           </div>
+        </div>
   
-          <div className="flex justify-start items-center gap-1 w-full text-sm">
-            <p>
-              Min: {Math.round(data.main.temp_min)}°
-            </p>
-            <p>
-              Max: {Math.round(data.main.temp_max)}°
+        <div className="w-full ml-4">
+          <div className="flex justify-start items-center gap-2 mb-2">
+            <img
+              className={cn("block w-12 h-9 object-cover object-center border rounded-sm bg-neutral-400 dark:border-neutral-700 dark:bg-transparent", isNightIcon && "bg-transparent dark:bg-neutral-300")}
+              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            />
+            <p className="">
+              {data.weather[0].main}
             </p>
           </div>
         </div>
