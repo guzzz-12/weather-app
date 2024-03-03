91118429dac1c095176d6f9f14c2084286ffaf41
unknown <jegq.dev.projects@gmail.com>
2024-03-03 10:57:34 -0400

    Card reutilizable con skeleton para renderizar los elementos de la interfaz

--- components/AirPollution.tsx
+++ components/AirPollution.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { Circle, SprayCan } from "lucide-react";
-import { Skeleton } from "./ui/skeleton";
+import { Circle } from "lucide-react";
+import ItemCard from "./ItemCard";
 import { AirPollutionData } from "@/types";
 import { cn } from "@/lib/utils";
 
@@ -12,72 +12,58 @@ interface Props {
 }
 
 const AirPollution = ({data, isLoading, error}: Props) => {
-  if (isLoading) {
-    return (
-      <Skeleton className="w-full h-24 rounded-sm border dark:border-neutral-700" />
-    )
-  }
-
   if (error) {
     return <p>{error}</p>
   }
 
-  if (data) {
-    let airQualityIndex = "good";
-    let position = "left-1";
+  let airQualityIndex = "good";
+  let position = "left-1";
 
-    // Determinar el índice de la calidad del aire y la posición del slider
-    switch (data.list[0].main.aqi) {
-      case 1:
-        airQualityIndex = "good";
-        position = "left-1";
-        break;
-      case 2:
-        airQualityIndex = "fair";
-        position = "left-[25%]";
-        break;
-        case 3:
-        airQualityIndex = "moderate";
-        position = "left-[50%]";
-        break;
-      case 4:
-        airQualityIndex = "poor";
-        position = "left-[75%]";
-        break;
-      case 5:
-        airQualityIndex = "very poor";
-        position = "left-[90%]";
-        break;
-      default:
-        airQualityIndex = "good";
-        position = "left-1";
-    }
-
-    return (
-      <div className="flex flex-col justify-between items-center w-full h-max px-3 py-2 flex-shrink-0 border dark:border-neutral-700 rounded-lg bg-neutral-100 dark:bg-neutral-950">
-        <div className="w-full mb-3 pb-2 border-b">
-          <div className="flex justify-start items-center gap-2">
-            <SprayCan className="text-neutral-500 dark:text-neutral-400" />
-            <p className="text-sm text-neutral-700 font-semibold dark:text-white">
-              Air Pollution
-            </p>
-          </div>
-        </div>
-  
-        <div className="pollution-gradient relative w-full h-4 mb-3 px-3 rounded-full">
-          <Circle
-            className={cn("absolute top-[50%] h-3.5 aspect-square text-neutral-700 dark:text-black fill-white -translate-y-[50%]", position)}
-          />
-        </div>
-
-        <p className="w-full ml-3 text-sm text-left">
-          Air quality is {airQualityIndex}
-        </p>
-      </div>
-    )
+  // Determinar el índice de la calidad del aire y la posición del slider
+  switch (data?.list[0].main.aqi) {
+    case 1:
+      airQualityIndex = "good";
+      position = "left-1";
+      break;
+    case 2:
+      airQualityIndex = "fair";
+      position = "left-[25%]";
+      break;
+      case 3:
+      airQualityIndex = "moderate";
+      position = "left-[50%]";
+      break;
+    case 4:
+      airQualityIndex = "poor";
+      position = "left-[75%]";
+      break;
+    case 5:
+      airQualityIndex = "very poor";
+      position = "left-[90%]";
+      break;
+    default:
+      airQualityIndex = "good";
+      position = "left-1";
   }
 
-  return null;
+  return (
+    <ItemCard
+      title="Air Pollution"
+      icon="SprayCan"
+      loading={isLoading}
+      loaderHeight="h-24"
+    >
+      <div className="pollution-gradient relative w-full h-4 mb-3 px-3 rounded-full">
+        <Circle
+          className={cn("absolute top-[50%] h-3.5 aspect-square text-neutral-700 dark:text-black fill-white -translate-y-[50%]", position)}
+        />
+      </div>
+
+      <p className="w-full ml-3 text-sm text-left">
+        Air quality is {airQualityIndex}
+      </p>
+    </ItemCard>
+  );
 }
 
 export default AirPollution;
---
+++ components/ItemCard.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import { icons } from "lucide-react";
+import { Skeleton } from "./ui/skeleton";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  icon: keyof typeof icons;
+  title: string;
+  loading: boolean;
+  loaderHeight: `h-${number}` | `h-[${number}px]` | `h-[${number}rem]`;
+  children: ReactNode;
+}
+
+const ItemCard = ({icon, title, loading, loaderHeight, children}: Props) => {
+  const Icon = icons[icon];
+
+  if (loading) {
+    return (
+      <Skeleton className={cn("w-full rounded-sm border dark:border-neutral-700", loaderHeight)} />
+    )
+  }
+
+  return (
+    <div className="flex flex-col justify-between items-center w-full h-max px-3 py-2 flex-shrink-0 border dark:border-neutral-700 rounded-lg bg-neutral-100 dark:bg-neutral-950">
+        <div className="w-full mb-3 pb-2 border-b">
+          <div className="flex justify-start items-center gap-2">
+            <Icon className="text-neutral-500 dark:text-neutral-400" />
+            <p className="text-sm text-neutral-700 font-semibold dark:text-white">
+              {title}
+            </p>
+          </div>
+        </div>
+  
+        {children}
+      </div>
+  )
+}
+
+export default ItemCard;
