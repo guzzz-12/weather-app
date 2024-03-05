d5c73508ea41f1184dade71807b4375efce75ad4
unknown <jegq.dev.projects@gmail.com>
2024-03-04 20:31:57 -0400

    Componente para mostrar el pronóstico de los próximos 5 días

---
+++ components/FiveDaysForecast.tsx
@@ -0,0 +1,59 @@
+import moment from "moment";
+import ItemCard from "./ItemCard";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+import { ApiErrorType, DailyForecast } from "@/types";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  data: DailyForecast | null;
+  isLoading: boolean;
+  error: string | null;
+  errorType: ApiErrorType | null;
+}
+
+const FiveDaysForecast = ({data, isLoading, error, errorType}: Props) => {
+  console.log({data});
+
+  return (
+    <ItemCard
+      title="5-Day Forecast"
+      icon="CalendarDays"
+      loading={isLoading}
+      error={error}
+      errorType={errorType}
+      loaderHeight="h-24"
+      item="dailyForecast"
+    >
+      <Carousel className="w-full max-w-xs py-2">
+        <CarouselContent className="flex justify-center items-center">
+          {data?.list.map((item, index) => (
+            <CarouselItem
+              key={index}
+              className="basis-1/2 h-full"
+            >
+              <div className="flex flex-col h-full">
+                <p className="mb-3 text-center text-sm">
+                  {moment(item.dt_txt).format("DD/MM - HH:mm")}
+                </p>
+
+                <div className="flex flex-col justify-center items-center gap-1 mt-auto">
+                  <img
+                    className={cn("block w-full aspect-square object-cover object-center border rounded-sm bg-neutral-400 dark:border-neutral-700 dark:bg-transparent")}
+                    src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                  />
+                  <p className="text-center text-sm">
+                    {item.weather[0].main}
+                  </p>
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </ItemCard>
+  )
+}
+
+export default FiveDaysForecast
